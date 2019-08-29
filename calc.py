e7b61da600c62368c17d412de3237baec987b0ba
RuchikaL11 <54612669+RuchikaL11@users.noreply.github.com>
2019-08-29 09:28:27 +0530

    C1

--- calc.py
+++ calc.py
@@ -5,7 +5,7 @@ def add(x,y):
         def multiply(x,y):
                 pass
             def divide(x,y):
-                pass
+                Pass
             def square(x,y):
                     pass
 
